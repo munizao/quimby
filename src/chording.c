2e9c6136cce73397b7e8993f537ee872b966e211
munizao <pz@puzzlezapper.com>
2014-06-24 18:20:16 -0700

    Starting branch for support for chording keys with space to form common English words.

---
+++ src/chording.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <glib.h>
+
+/* Support for chorded input */
+
+static gboolean
+word_start_context (gchar *text, gint cursor_index)
+{
+    gchar *text;
+    gint cursor_index;
+    gboolean is_start = false;
+    gunichar prev = g_utf8_get_char(text+cursor_index);
+    gunichar space = 0x20;
+    if (prev == space) /* Actual algorithm will be more complicated. */
+    {
+	is_start = true;
+    }
+    return is_start;
+}
+
